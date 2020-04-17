13485b20abfc3ea8ee1fcc6139ff01320baa40d9
DESKTOP-MMKDHVN\Estera <estera.robu@gmail.com>
2020-04-17 14:50:26 +0300

    Packeging decorators

--- Store-Management-App/Store-Management-App/Decorators/HardwereDecorator.cs
+++ Store-Management-App/Store-Management-App/Decorators/HardwereDecorator.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Store_Management_App.Model
+namespace Store_Management_App.Decorators
 {
     class HardwereDecorator : ProviderDecorator
     {
